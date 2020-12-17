7e487db6d919bf6f532b88f63fc85a5a46c89c0e
BingYu <33444256+BingYu94860@users.noreply.github.com>
2020-12-17 10:58:52 +0800

    update 12/15

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
 from mainsite.models import Post, AccessInfo, Branch, StoreIncome
 from datetime import datetime
 import random
+
 
 def homepage(request):
     rec = AccessInfo()
@@ -14,14 +14,34 @@ def homepage(request):
     return render(request, "index.html", locals())
 
 
-def mychart(request,branchid=0):
+def mychart(request, branchid=0):
     now = datetime.now()
-    branches = Branch.objects.all()
+    stores = Branch.objects.all()
     if branchid == 0:
         data = StoreIncome.objects.all()
     else:
         # branchid = Branch.objects.get(title="左營店")
         data = StoreIncome.objects.filter(branch=branchid)
+    title = "各分店營收情形"
+    return render(request, "mychart.html", locals())
+
+def chart(request, year=0, month=0):
+    now = datetime.now()
+    stores = Branch.objects.all()
+
+    if year == 0:
+        data = StoreIncome.objects.all()
+    else:
+        data = StoreIncome.objects.filter(income_year=year)
+        if month > 0:
+            data = data.filter(income_month=month)
+
+    if year > 0 and month > 0:
+        title = "{}年{}月各分店營收情形".format(year, month)
+    elif year > 0:
+        title = "{}年各分店營收情形".format(year)
+    else:
+        title = "各分店營收情形"
     return render(request, "mychart.html", locals())
 
 
